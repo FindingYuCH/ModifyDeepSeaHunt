f0bd2b22b84b1112eb5654f4470f48d72bdd5ef0
m3tech akn <m3techakn@m3techmatoiMac.local>
2012-05-03 17:49:21 +0800

    Initial Commit

---
+++ DeepSeaHunt/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  DeepSeaHunt
+//
+//  Created by akn m3tech on 12-5-3.
+//  Copyright akn 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ DeepSeaHunt/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  DeepSeaHunt
+//
+//  Created by akn m3tech on 12-5-3.
+//  Copyright akn 2012年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
